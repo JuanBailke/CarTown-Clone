d863c1c29110663c4b5ef2aca4a6f814f3bf8705
Juan Bailke <juan_bailke@hotmail.com>
2023-09-07 22:49:12 -0300

    Loja concluída com criação de carros e elevadores, juntamente da validação de dinheiro

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,20 +14,25 @@ public class GameManager : MonoBehaviour
     public GameObject panelItems;
     public GameObject main;
     public Text coinTxt;
+    public Text coinTxtCarShop;
+    public Text coinTxtItemShop;
     public Text levelTxt;
     public Transform garage;
     public GameObject btnShop;
 
     [Header("Canvas Loja")]
-    public Text name;
-    public Text value;
-    public Button buttonBuy;
+    public Text nameCar;
+    public Text valueCar;
+    public Text nameItem;
+    public Text valueItem;
+    public Button buttonCarBuy;
+    public Button buttonItemBuy;
     public GameObject panel;
     private GameObject copy;
 
     //Variáveis de Gameplay
-    private int coin, totalCoin;
-    private GameObject copyCar;
+    private int coin, valueTrade;
+    private GameObject copyObject;
     public bool create;
 
     // Start is called before the first frame update
@@ -36,6 +41,8 @@ public class GameManager : MonoBehaviour
         panelShop.SetActive(false);
         panelCars.SetActive(false);
         panelItems.SetActive(false);
+
+        coin = int.Parse(coinTxt.text);
     }
 
     // Update is called once per frame
@@ -44,38 +51,75 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public void getCoins(int qtd)
+    public void setCoins(int qtd)
     {
         coin += qtd;
     }
 
     public void CallShop(Carro car)
     {
-        name.text = car.carNome;
-        value.text = "Valor: " + car.carValue;
-        copyCar = car.carObject;
-    }
-
-    public void EnableBuy()
-    {
-        buttonBuy.enabled = true;
+        nameCar.text = car.carNome;
+        valueCar.text = "Valor: " + car.carValue;
+        copyObject = car.carObject;
+        if (coin >= car.carValue)
+        {
+            buttonCarBuy.enabled = true;
+            valueTrade = car.carValue;
+        }
+        else
+        {
+            buttonCarBuy.enabled = false;
+        }
     }
 
     public void CreateObjectInScene()
     {
-        GameObject objInst = Instantiate(copyCar, new Vector3(-20, 0.1f, -20), Quaternion.Euler(0, 270, 0), garage);
+        GameObject objInst = Instantiate(copyObject, new Vector3(-20, 0.1f, -20), Quaternion.Euler(0, 270, 0), garage);
         create = true;
         panelShop.SetActive(false);
+        panelCars.SetActive(false);
         main.SetActive(true);
+        coin -= valueTrade;
+        coinTxt.text = coin.ToString();
+        nameCar.text = "Selecione um item";
+        valueCar.text = "";
+    }
 
-        //Camera.main.ScreenToViewportPoint(Input.mousePosition)
+    public void CallShop(Itens item)
+    {
+        nameItem.text = item.itemNome;
+        valueItem.text = "Valor: " + item.itemValue;
+        copyObject = item.itemObject;
+        if (coin >= item.itemValue)
+        {
+            buttonItemBuy.enabled = true;
+            valueTrade = item.itemValue;
+        }
+        else
+        {
+            buttonItemBuy.enabled = false;
+        }
+    }
+
+    public void CreateItemInScene()
+    {
+        GameObject objInst = Instantiate(copyObject, new Vector3(-20, 0.1f, -20), Quaternion.Euler(-180, 0, 0), garage);
+        create = true;
+        panelShop.SetActive(false);
+        panelItems.SetActive(false);
+        main.SetActive(true);
+        coin -= valueTrade;
+        coinTxt.text = coin.ToString();
+        nameItem.text = "Selecione um item";
+        valueItem.text = "";
     }
 
     public void OpenShop()
     {
         panelShop.SetActive(true);
         main.SetActive(false);
-        
+        coinTxtCarShop.text = coinTxt.text;
+        coinTxtItemShop.text = coinTxt.text;
     }
 
     public void CloseShop()
@@ -87,11 +131,26 @@ public class GameManager : MonoBehaviour
     public void OpenShopCars()
     {
         panelCars.SetActive(true);
-        buttonBuy.enabled = false;
+        buttonCarBuy.enabled = false;
     }
 
     public void CloseShopCars()
     {
         panelCars.SetActive(false);
+        nameCar.text = "Selecione um item";
+        valueCar.text = "";
+    }
+
+    public void OpenShopItems()
+    {
+        panelItems.SetActive(true);
+        buttonItemBuy.enabled = false;
+    }
+
+    public void CloseShopItems()
+    {
+        panelItems.SetActive(false);
+        nameItem.text = "Selecione um item";
+        valueItem.text = "";
     }
 }
---
+++ Assets/Scripts/Itens.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Itens : MonoBehaviour
+{
+    public int itemId;
+    public string itemNome;
+    public int itemValue;
+    public GameObject itemObject;
+}
